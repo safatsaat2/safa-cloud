3b7147353923b8a94ba7fea1dfeb085889a59b40
unknown <sakibsafat47@gmail.com>
2025-02-25 10:49:49 +0600

    constants creation for static data

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -9,6 +9,7 @@ const Sidebar = () => {
                 <Link href='/' className='sidebar-logo'>
                     <Image src='/assets/images/logo-text.svg' alt='logo' width={180} height={28} />
                 </Link>
+                
             </div>
         </aside>
     );
