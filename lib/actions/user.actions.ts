0faf979eccfbb56b3fd07d4efd56174b5a071723
unknown <sakibsafat47@gmail.com>
2025-02-25 13:22:11 +0600

    User update implementation

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,5 @@
 "use server";
 
-
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
@@ -20,15 +19,32 @@ export async function createUser(user: CreateUserParams) {
 
 // READ
 export async function getUserById(userId: string) {
-    try {
-      await connectToDatabase();
-  
-      const user = await User.findOne({ clerkId: userId });
-  
-      if (!user) throw new Error("User not found");
-  
-      return JSON.parse(JSON.stringify(user));
-    } catch (error) {
-      handleError(error);
-    }
+  try {
+    await connectToDatabase();
+
+    const user = await User.findOne({ clerkId: userId });
+
+    if (!user) throw new Error("User not found");
+
+    return JSON.parse(JSON.stringify(user));
+  } catch (error) {
+    handleError(error);
   }
+}
+
+// UPDATE
+export async function updateUser(clerkId: string, user: UpdateUserParams) {
+  try {
+    await connectToDatabase();
+
+    const updatedUser = await User.findOneAndUpdate({ clerkId }, user, {
+      new: true,
+    });
+
+    if (!updatedUser) throw new Error("User update failed");
+
+    return JSON.parse(JSON.stringify(updatedUser));
+  } catch (error) {
+    handleError(error);
+  }
+}
