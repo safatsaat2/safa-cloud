1a53eb962899187d72d8c079edc7f55276334f3e
unknown <sakibsafat47@gmail.com>
2025-02-25 13:26:03 +0600

    utils file update

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
@@ -42,51 +44,50 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     });
 
     if (!updatedUser) throw new Error("User update failed");
-
+    
     return JSON.parse(JSON.stringify(updatedUser));
   } catch (error) {
     handleError(error);
   }
 }
 
-
 // DELETE
 export async function deleteUser(clerkId: string) {
-    try {
-      await connectToDatabase();
-  
-      // Find user to delete
-      const userToDelete = await User.findOne({ clerkId });
-  
-      if (!userToDelete) {
-        throw new Error("User not found");
-      }
-  
-      // Delete user
-      const deletedUser = await User.findByIdAndDelete(userToDelete._id);
-      revalidatePath("/");
-  
-      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
-    } catch (error) {
-      handleError(error);
-    }
-  }
+  try {
+    await connectToDatabase();
 
-  // USE CREDITS
-export async function updateCredits(userId: string, creditFee: number) {
-    try {
-      await connectToDatabase();
-  
-      const updatedUserCredits = await User.findOneAndUpdate(
-        { _id: userId },
-        { $inc: { creditBalance: creditFee }},
-        { new: true }
-      )
-  
-      if(!updatedUserCredits) throw new Error("User credits update failed");
-  
-      return JSON.parse(JSON.stringify(updatedUserCredits));
-    } catch (error) {
-      handleError(error);
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) {
+      throw new Error("User not found");
     }
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+    revalidatePath("/");
+
+    return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+  } catch (error) {
+    handleError(error);
   }
+}
+
+// USE CREDITS
+export async function updateCredits(userId: string, creditFee: number) {
+  try {
+    await connectToDatabase();
+
+    const updatedUserCredits = await User.findOneAndUpdate(
+      { _id: userId },
+      { $inc: { creditBalance: creditFee }},
+      { new: true }
+    )
+
+    if(!updatedUserCredits) throw new Error("User credits update failed");
+
+    return JSON.parse(JSON.stringify(updatedUserCredits));
+  } catch (error) {
+    handleError(error);
+  }
+}
