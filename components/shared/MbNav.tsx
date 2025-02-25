732aeb020808aef69bdd99734232f8beb9b25fc8
unknown <sakibsafat47@gmail.com>
2025-02-25 11:25:42 +0600

    logo implementation on mobile navbar

--- components/shared/MbNav.tsx
+++ components/shared/MbNav.tsx
@@ -1,10 +1,23 @@
+import Link from 'next/link';
 import React from 'react';
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+import Image from 'next/image';
+  
 
 const MbNav = () => {
     return (
-        <div>
-            
-        </div>
+        <header className='header'>
+            <Link href={'/'} className='flex items-center gap-2 md:py-2'>
+                <Image src="/assets/images/logo-text.svg" alt='logo' width={180} height={28}/>
+            </Link>
+        </header>
     );
 };
 
