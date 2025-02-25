4f39c648b1f59e238dfaf47d0b96f45f1e4c00de
unknown <sakibsafat47@gmail.com>
2025-02-25 13:22:42 +0600

    user deletation action implementation

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -48,3 +48,26 @@ export async function updateUser(clerkId: string, user: UpdateUserParams) {
     handleError(error);
   }
 }
+
+
+// DELETE
+export async function deleteUser(clerkId: string) {
+    try {
+      await connectToDatabase();
+  
+      // Find user to delete
+      const userToDelete = await User.findOne({ clerkId });
+  
+      if (!userToDelete) {
+        throw new Error("User not found");
+      }
+  
+      // Delete user
+      const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+      revalidatePath("/");
+  
+      return deletedUser ? JSON.parse(JSON.stringify(deletedUser)) : null;
+    } catch (error) {
+      handleError(error);
+    }
+  }
