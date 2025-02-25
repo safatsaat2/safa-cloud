ae44f5c00796addca948e01d3c0527bb3b019d35
unknown <sakibsafat47@gmail.com>
2025-02-25 11:14:21 +0600

    separating item on list of nav

--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -20,7 +20,7 @@ const Sidebar = () => {
                 <nav className="sidebar-nav">
                     <SignedIn>
                         <ul className="sidebar-nav_elements">
-                            {navLinks.map((link)=>{
+                            {navLinks.slice(0, 6).map((link)=>{
                                 const isActive = link.route === pathname
                                 return(
                                     <li key={link?.route} className={`sidebar-nav_element group ${isActive? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
@@ -31,7 +31,20 @@ const Sidebar = () => {
                                     </li>
                                 )
                             })}
-                            <li className="flex-center cursor-pointer gap-2 p4">
+                        </ul>
+                        <ul className="sidebar-nav_elements">
+                        {navLinks.slice(6).map((link)=>{
+                                const isActive = link.route === pathname
+                                return(
+                                    <li key={link?.route} className={`sidebar-nav_element group ${isActive? 'bg-purple-gradient text-white' : 'text-gray-700'}`}>
+                                        <Link href={link?.route} className='sidebar-link'>
+                                        <Image src={link?.icon} alt='logo' width={24} height={24} className={` ${isActive && 'brightness-200'} `} />
+                                        {link?.label}
+                                        </Link>
+                                    </li>
+                                )
+                            })}
+                            <li className="flex-center cursor-pointer gap-2 p4 ">
                                 <UserButton afterSwitchSessionUrl='/' showName/>
                             </li>
                         </ul>
