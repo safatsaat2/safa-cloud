4cac0160b238d1e8f98b3e6f2a1084ae2debf8f1
unknown <sakibsafat47@gmail.com>
2025-03-08 13:15:23 +0600

    implement clerk webhook

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -61,13 +61,13 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      clerkId: id,
-      email: email_addresses[0].email_address,
-      username: username!,
-      firstName: first_name,
-      lastName: last_name,
-      photo: image_url,
-    };
+        clerkId: id,
+        email: email_addresses[0].email_address,
+        username: username!,
+        firstName: first_name ?? "",  // Ensure it's always a string
+        lastName: last_name ?? "",    // Ensure it's always a string
+        photo: image_url,
+      };
 
     const newUser = await createUser(user);
 
@@ -88,8 +88,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name ?? "",
+      lastName: last_name ?? "",
       username: username!,
       photo: image_url,
     };
