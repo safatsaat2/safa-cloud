7ae035c74ef2241c4c56edc21702ea016ed84044
unknown <sakibsafat47@gmail.com>
2025-03-09 17:18:50 +0600

    react hook form implementation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,13 +1,17 @@
 import Header from '@/components/shared/Header';
+import TranformationForm from '@/components/shared/TranformationForm';
 import { transformationTypes } from '@/constants';
 import React from 'react';
 
 const AddTransformationTypePage = ({params: {type}} : SearchParamProps) => {
     const transformation = transformationTypes[type];
     return (
+        <>
         <Header title={transformation.title}
         subTitle={transformation.subTitle}
         />
+        <TranformationForm/>
+        </>
     );
 };
 
